06c760ed395136084a55fddc86a97683afec3ea8
Eplundare <aelozanoa@gmail.com>
2021-03-02 18:03:55 +0100

    Added translator and played with Light Settings

---
+++ Assets/Scripts/Translator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Translator : MonoBehaviour
+{
+    public float forwardSpeed;
+    public Vector3 forwardVector;
+
+    public void Start()
+    {
+        forwardSpeed = -2f;
+        forwardVector = new Vector3(0f, 0f, forwardSpeed);
+    }
+
+    public void Update()
+    {
+        transform.Translate(forwardVector * Time.deltaTime, Space.Self);
+    }
+}
